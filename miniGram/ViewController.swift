92e79932ca476d31fb3199b67ed468d0579559e1
Ashiq Uz Zoha <ashiq.ayon@gmail.com>
2023-04-08 23:09:07 +0600

    Initial Commit

---
+++ miniGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniGram
+//
+//  Created by Ashiq Uz Zoha on 8/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
