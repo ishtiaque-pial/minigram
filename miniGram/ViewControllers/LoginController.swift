26227712a32b5ed6977d1d60004abab70311b2e7
Ashiq Uz Zoha <ashiq.ayon@gmail.com>
2023-05-07 23:59:58 +0600

    validation codes

--- miniGram/ViewControllers/LoginController.swift
+++ miniGram/ViewControllers/LoginController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     
     
     override func viewDidLoad() {
@@ -17,9 +19,26 @@ class LoginController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func validate () -> Bool {
+        guard let email = self.emailField.text, isValidEmail(email: email) else {
+            return false
+        }
+        return true
+    }
+    
+    func isValidEmail (email: String) -> Bool {
+        // write your email validation code, return true if valid
+        return true
+    }
+    
 
     @IBAction func onClickLoginButton () {
         print("login button was clicked")
+        
+        let validated = self.validate()
+        if validated == false {
+            return 
+        }
         
         if let homeTabBarController = self.storyboard?.instantiateViewController(withIdentifier: MinigramApp.homeTabBarController) as? UITabBarController {
             
